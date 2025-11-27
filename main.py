6de4de267070c060d1c955a71e8afc873e8fd363
mikhail-maistrenko <rndmikle@mail.ru>
2025-11-27 12:54:00 +0300

    Lab 6

--- main.py
+++ main.py
@@ -1,4 +1,81 @@
 import math
+import random
+# Ввод количества слов
+while True:
+    try:
+        n = int(input("Введите количество слов в тексте: "))
+        if n > 0:
+            break
+        else:
+            print("Число должно быть больше 0")
+    except ValueError:
+        print("Введите целое число")
+# Генерация текста
+letters = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+words = []
+for i in range(n):
+    length = random.randint(3, 10)
+    word = ''
+    for x in range(length):
+        word += random.choice(letters)
+    words.append(word)
+text = ' '.join(words)
+print(f"Tекст: {text}")
+# Анализ первых букв
+letter_count = {}
+for word in words:
+    if word:
+        first_letter = word[0].lower()
+        if first_letter in letter_count:
+            letter_count[first_letter] += 1
+        else:
+            letter_count[first_letter] = 1
+# Поиск буквы с максимальным количеством слов
+max_letter = ''
+max_count = 0
+for letter, count in letter_count.items():
+    if count > max_count:
+        max_letter = letter
+        max_count = count
+# Вывод результатов
+print(f"Всего слов: {len(words)}")
+print("Статистика:")
+
+for letter in sorted(letter_count.keys()):
+    print(f"'{letter}': {letter_count[letter]}")
+
+print(f"Результат: больше всего слов начинается на букву '{max_letter}'")
+print(f"Количество: {max_count}")
+# print("Задача 2")
+# while True:
+#     text = input("Введите строку: ").strip()
+#     if text:
+#         break
+#     print("Строка не может быть пустой!")
+#
+# print(f"Cтрока: {text}")
+#
+# latin = 0
+# russian = 0
+#
+# try:
+#
+#     for char in text:
+#         if char in '0123456789':
+#             test = int("эта строка содержит цифры")
+#
+#         if 'a' <= char <= 'z':
+#             latin += 1
+#         elif 'а' <= char <= 'я':
+#             russian += 1
+#
+#     # print(f"Латинских: {latin}")
+#     # print(f"Русских: {russian}")
+#     print(f"Общее количество букв: {latin + russian}")
+#
+# except ValueError:
+#     print("В строке должны быть только буквы, попробуйте снова ")
+
 # print("Задача 1")
 # while True:
 #     try:
@@ -17,44 +94,44 @@ import math
 # print("Количество месяцев К:",K)
 # print("Итоговый размер вклада S:",S)
 # # ---------------------------------------------------------------
-print("Задача 2")
-while True:
-    try:
-        x0 = float(input("Введите х0: "))
-        break
-    except ValueError:
-        print("Это не число. Попробуйте снова")
-
-while True:
-    try:
-        x1 = float(input("Введите х1: "))
-        break
-    except ValueError:
-        print("Это не число. Попробуйте снова")
-
-while True:
-    try:
-        dx = float(input("Введите dx: "))
-        break
-    except ValueError:
-        print("Это не число. Попробуйте снова")
-
-if x0 < x1 and dx > 0:
-    x = x0
-    while x < x1:
-        if x < 0:
-            z = x**2
-        elif 0 < x <= 1:
-            z = math.sin(x)
-        else:
-            z = math.cos(x)
-
-        x += dx
-        print("z =",z)
-elif x0 > x1 and dx < 0:
-    print(1)
-else:
-    print("ERROR")
+# print("Задача 2")
+# while True:
+#     try:
+#         x0 = float(input("Введите х0: "))
+#         break
+#     except ValueError:
+#         print("Это не число. Попробуйте снова")
+#
+# while True:
+#     try:
+#         x1 = float(input("Введите х1: "))
+#         break
+#     except ValueError:
+#         print("Это не число. Попробуйте снова")
+#
+# while True:
+#     try:
+#         dx = float(input("Введите dx: "))
+#         break
+#     except ValueError:
+#         print("Это не число. Попробуйте снова")
+#
+# if x0 < x1 and dx > 0:
+#     x = x0
+#     while x < x1:
+#         if x < 0:
+#             z = x**2
+#         elif 0 < x <= 1:
+#             z = math.sin(x)
+#         else:
+#             z = math.cos(x)
+#
+#         x += dx
+#         print("z =",z)
+# elif x0 > x1 and dx < 0:
+#     print(1)
+# else:
+#     print("ERROR")
 
  #-------------------------------------------------------------
 # print("Задача 3")
